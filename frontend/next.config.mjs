077e9c0447c4f1c06545724c29b87a453b0bb14a
rizkirmdhn1215 <rizkirmdhn1215@gmail.com>
2026-02-21 16:23:04 +0700

    newss

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://basarnas-backend:4000/api/:path*',
+        destination: 'http://basarnas-backend:4000/:path*',
       },
     ];
   },
